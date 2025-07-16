d00f75c2b7021436db3d74bcf8348234089b914e
Cristiane11 <bjwmcd@yahoo.com>
2025-07-16 08:16:26 -0400

    update

--- todo-list-app/src/FunctionClassComponent.jsx
+++ todo-list-app/src/FunctionClassComponent.jsx
@@ -1,18 +1,18 @@
-import {Component}from 'react';
+
 import { useState } from 'react';
 
 
 function FunctionClassComponent(){
     const[message, setMessage] = useState("welcome to the Class")
   
-    
-    
-  
+    const changeMessage =()=>{
+        setMessage('Tenha um Bom Dia!')
+    }
     return (
         <div>
             <h1>Hello from Class Component</h1>
             <p>{message}</p>
-            <button onClick={() => setMessage('Tenha um Bom Dia!')}>Change Message</button>
+            <button onClick={changeMessage}>Change Message</button>
         </div>
     );
 }
