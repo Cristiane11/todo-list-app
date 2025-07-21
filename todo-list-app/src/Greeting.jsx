ce2543fc7f495f1a3ca6735063781483b7572919
Cristiane11 <bjwmcd@yahoo.com>
2025-07-21 10:50:55 -0400

    update

--- todo-list-app/src/Greeting.jsx
+++ todo-list-app/src/Greeting.jsx
@@ -3,17 +3,34 @@ import { useState } from 'react';
 import {string} from 'prop-types'
 
 
-function Greeting = (props) => {
+const Greeting = (props) => {
     const[name, setName] = useState(props.initialName);
     const[isLoggedIn, setIsLoggedIn] = useState(false);
+    const[user, setUsers] = useState(['Alice','Bob','Charlie','John'])
     
+    const handleLoginClick =()=>{
+        setIsLoggedIn(true);
+    }
+
+    const handleLogOutClick =()=>{
+        setIsLoggedIn(false);
+        setName(props.initialName)
+    }
     return (
         <div>
+            <p>
+                {props.customMessage}
+            </p>
+            <ul>
+                {user.map((user, index)=>(
+                    <li key={index}>{user}</li>
+                ))}
+            </ul>
         <p>
-        {/*expression ? <true>:<false>*/}
+        {/*expression  false ? <true>:<false>*/}
         {isLoggedIn ? `Hello, ${name} ! Welcome back !` : props.customMessage}
         </p>
-        {/* expression is false ?ask to login: ask to logout*/ }
+        {/* expression is false ? ask to login: ask to logout*/ }
         {!isLoggedIn ? (
             <button onClick ={handleLoginClick}>Log in</button>
         ) : (
